52f75ea59cf76a15adec71fe732bc2a06ddc18e0
Gaurigithub-p <gauripawar8692@gmail.com>
2025-07-21 16:12:12 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,1 +1,7 @@
-ii
+terraform {
+  backend "s3" {
+    bucket = "chatbot-bucket9152" # Replace with your actual S3 bucket name
+    key    = "project/infra/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
